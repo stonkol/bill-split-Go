8a6b9edffc8e9f94ab4b24a8c908b0eddecb3f4a
stonkol <hector96fi@yahoo.es>
2024-03-11 18:10:54 +0100

    retrieve person and currency

--- bill-split-app.go
+++ bill-split-app.go
@@ -19,17 +19,18 @@ type Person struct {
 }
 
 type Item struct {
-	name        string
-	category    string
-	price       float32
-	currency    string
+	//	name        string
+	category string
+	price    float32
+	//	currency    string
 	full_amount bool
 }
 
-type CurrencyFormat struct {
-	full_name    string
+type Currency struct {
+	//	full_name    string
 	three_digits string
 	one_digit    string
+	total        float32
 }
 
 func finalPrint(p1_lent, p2_lent float32, p1_name, p2_name, currency string) {
@@ -52,8 +53,8 @@ func main() {
 	// currency's acronyms
 	const (
 		e string = "eur"
-		j string = "jpy"
-		p string = "gbp"
+	//	j string = "jpy"
+	//	p string = "gbp"
 	)
 
 	// currency exchange
@@ -64,61 +65,17 @@ func main() {
 	)
 
 	// add all the same currencies
-	var total, total_eur, total_gbp, total_jpy float32 = 0.0, 0.0, 0.0, 0.0
+	// var total, total_eur, total_gbp, total_jpy float32 = 0.0, 0.0, 0.0, 0.0
 
 	//////////////////////////////
 	/////// RETRIEVE LIST ////////
 	//////////////////////////////
 	const default_currency = "eur"
 
-	//var item []Item
-	//  item := Item{
-	//	  name:       "Default Item",
-	//	  price:      0.0,
-	//	  currency:   "EUR",
-	//	  category:   "other",
-	//	  full_amount: false,
-	//}
-
-	// // map imput list
-	// var p, i int16 := 0, 0
-	// if first letter is "f" {
-	//    item
-	// } else if first letter is num {
-	//    item
-	// }
-	// } else if "#" first letter {
-	//   person[p].name = "(string after "# ")"
-	//   i++
-	// } else if "##" first 2 letters{
-	//   item[i].currency = (the part after "## ")
-	//
-	// // calculate it
-	// for p := 0; p <=10; p++ {
-	//   if detect new "#" {
-	//
-	//     for item[i] {}
-	//         if  {}
-	//         total += item[i].price
-	//     p++
-	//   } else if "page end" {
-	//       p = 11
-	//   }
-	// }
-
-	// detect the first '#', it will be the first user_1
-
-	// add money on user_1_lent
-
-	// if detect another "#", then +1 to person[i] will start
-
-	// is adding money to each currency
-
 	//////////////////////////////
 	/////////////////////////////
 	//////// OPEN MD ////////////
-	// Open the markdown file
-	file, err := os.Open("the-bills/test-2p-2i.md")
+	file, err := os.Open("the-bills/test-2p-2c-2i.md")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -131,18 +88,66 @@ func main() {
 	// Define slices to store persons and lent amounts
 	person := make([]Person, 0)
 
+	p := -1
 	// Read each line of the file
 	for scanner.Scan() {
 		line := scanner.Text()
+
+		///////////// PERSON ///////////////
 		if strings.HasPrefix(line, "# ") {
+			p++
+
 			// Extract the name from the line and create a new Person
 			name := strings.TrimSpace(strings.TrimPrefix(line, "# "))
 			person = append(person, Person{name: name})
-			fmt.Println("detected:", person)
+			fmt.Println("\n p, name, line:", p, name, line)
+			// person[p].name = name
+			// fmt.Println("detected:", line)
+
+			// // CURRENCY
+			// for {
+			// 	if strings.HasPrefix(line, "## ") {
+			// 		currency := make([]Currency, 0)
+			//
+			// 		three_digits := strings.TrimSpace(strings.TrimPrefix(line, "## "))
+			// 		currency = append(currency, Currency{three_digits: three_digits})
+			// 		fmt.Println("new currency:", line)
+			//
+			// 		// detect a blank line
+			// 	} else if len(strings.TrimSpace(line)) == 0 {
+			// 		// calculate total
+			//
+			// 		break
+			// 	}
+			// }
+			//////////////////  CURRENCY /////////////////////
+		} else if strings.HasPrefix(line, "## ") {
+			currency := make([]Currency, 0)
+
+			three_digits := strings.TrimSpace(strings.TrimPrefix(line, "## "))
+			currency = append(currency, Currency{three_digits: three_digits})
+
+			fmt.Println("new currency:", three_digits)
+
+			// currencyScanner := bufio.NewScanner(strings.NewReader(line))
+			itemScanner := bufio.NewScanner(strings.NewReader(line))
+			for itemScanner.Scan() {
+				// currencyLine := currencyScanner.Text()
+				itemLine := itemScanner.Text()
+				if len(strings.TrimSpace(itemLine)) == 0 {
+					person[p].lent = currency[p].total
+					break
+				}
+			}
 		}
+		// else if len(strings.TrimSpace(line)) == 0 {
+		//			// calculate total
+		//    break
+		//	}
 	}
 
 	// Print the names of the people
+	fmt.Println("______________")
 	for p := 0; p < len(person); p++ {
 		fmt.Printf("Person %d: %s\n", p+1, person[p].name)
 	}
@@ -188,7 +193,7 @@ func main() {
 	//	person[0].name = "Elephant"
 	//	person[1].name = "Mamut"
 
-	currency := default_currency
+	//	currency := default_currency
 
 	//  if currency == "eur" {
 	//    total_eur += item[i].price // add to eur
@@ -198,12 +203,55 @@ func main() {
 	//    fmt.Println("Error: No currency detected.")
 	//  }
 
-	total += total_eur
-	total += total_gbp * pte
-	total += total_jpy * jte
+	// total += total_eur
+	// total += total_gbp * pte
+	// total += total_jpy * jte
 
 	////////////////////////////////
 	////////////////////////////////
 
-	finalPrint(person[0].lent, person[1].lent, person[0].name, person[1].name, currency)
+	finalPrint(person[0].lent, person[1].lent, person[0].name, person[1].name, default_currency)
 }
+
+//var item []Item
+//  item := Item{
+//	  name:       "Default Item",
+//	  price:      0.0,
+//	  currency:   "EUR",
+//	  category:   "other",
+//	  full_amount: false,
+//}
+
+// // map imput list
+// var p, i int16 := 0, 0
+// if first letter is "f" {
+//    item
+// } else if first letter is num {
+//    item
+// }
+// } else if "#" first letter {
+//   person[p].name = "(string after "# ")"
+//   i++
+// } else if "##" first 2 letters{
+//   item[i].currency = (the part after "## ")
+//
+// // calculate it
+// for p := 0; p <=10; p++ {
+//   if detect new "#" {
+//
+//     for item[i] {}
+//         if  {}
+//         total += item[i].price
+//     p++
+//   } else if "page end" {
+//       p = 11
+//   }
+// }
+
+// detect the first '#', it will be the first user_1
+
+// add money on user_1_lent
+
+// if detect another "#", then +1 to person[i] will start
+
+// is adding money to each currency
